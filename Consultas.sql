39460f8a956ab180020efb3babf6d2538b54fff9
alfonm8 <61537992+alfonm8@users.noreply.github.com>
2021-04-17 16:20:27 +0200

    Añadidas más consultas

--- Consultas.sql
+++ Consultas.sql
@@ -61,3 +61,79 @@ CREATE VIEW StockMAY5 AS
     FROM Producto_TAB
     WHERE Stock<5;
 
+
+-- Alfonso
+-- 1.- Mostrar los Mecánicos que actualmente estén en periodo de contratación y haya realizado una reparación 
+
+create view mecrep as				
+select * from
+	(select * from
+		(select f.mecanico.dni as dni, f.mecanico.nombre as nombre, f.mecanico.empresa as empresa from factura_tab f)
+		intersect
+		(select dni, nombre, empresa from mecanico_tab
+		where periodo > sysdate))
+	natural join 
+	(select  f.mecanico.dni as dni, f.mecanico.nombre as nombre, f.mecanico.empresa as empresa, count(f.mecanico.dni) as facturas_totales
+	from factura_tab f
+	group by f.mecanico.dni, f.mecanico.nombre, f.mecanico.empresa
+	having count(f.mecanico.dni) > 0)
+
+
+--2.- Mostrar todos los vehículos eléctricos cuya autonomia sea superior a 190km y la persona que lo repara es 'Fernando'
+
+create view autonomia as
+select treat(value(v) as vehelectrico_obj).matricula as matricula , treat(value(v) as vehelectrico_obj).autonomia as autonomia, 
+treat(value(v) as vehelectrico_obj).emisiones as emisiones ,v.modelo, v.marca, v.disponibilidad
+from vehiculo_tab v
+where treat(value(v) as vehelectrico_obj).matricula <> 'null'  
+and treat(value(v) as vehelectrico_obj).autonomia > 190
+and v.mecanico.nombre = (select nombre from mecanico_tab
+                         where nombre = 'Fernando')
+
+
+--3.- Mostrar las facturas del vehículo con matrícula x y reparados por el mecánico y.
+
+create view factmec as
+select f.id_factura, f.descripcion, f.importe, f.mecanico.dni, f.vehiculo.matricula 
+from FACTURA_TAB f
+where f.mecanico.dni in (select dni from MECANICO_TAB where dni = '56217971E' ) 
+      and
+      f.vehiculo.matricula in (select matricula from VEHICULO_TAB where matricula = '4444jkl')
+
+
+--4.- Mostrar los vehículos que están en uso (Pedido en camino).
+
+create view vuso as
+select * from
+	(select * from
+		(select r.id_usuario from repartidor_tab r)
+		intersect
+		(select p.repartidor.id_usuario as idup from pedido_tab p
+		where p.estado = 'en camino'))
+	natural join
+	(select re.id_usuario, re.vehiculo.matricula as matricula from repartidor_tab re);
+ 
+
+-- 5.- Mostrar los restaurantes que tengan carne como producto y se localicen en Cuenca o Albacete
+-- y el producto tenga al menos una oferta. SIN TERMINAR
+
+create view pcca as
+(select p.restaurante.nombre, p.restaurante.ciudad from producto_tab p
+where p.tipo_producto = 'Carne' )
+intersect
+(select nombre, ciudad from restaurante_tab
+where ciudad = 'Cuenca' or ciudad = 'Albacete')
+
+
+--6.- Sumar todos los productos de los restaurantes pertenecientes a Albacete.
+
+create view sumaab as
+select count(p.id_producto), p.restaurante.ciudad 
+from producto_tab p
+where p.restaurante.ciudad = 'Albacete'
+group by p.restaurante.ciudad
+Having count(p.id_producto) > 0;
+
+
+
+
