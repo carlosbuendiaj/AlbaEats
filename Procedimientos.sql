7224fcecdbff498a95e40ef3b6d13b1caa227b9f
albertonov <72156084+albertonov@users.noreply.github.com>
2021-04-24 15:44:45 +0200

    Update Procedimientos.sql

--- Procedimientos.sql
+++ Procedimientos.sql
@@ -1,8 +1,8 @@
 --Dado el id de un pedido, calcula el precio total del pedido (sumatorio de los importes de cada pedido * cantidad + iva añadido) y actualiza el pedido con este valor.
 
-create or replace PROCEDURE ACTUALIZAR_PRECIO_PEDIDO (ID_PED NUMBER) AS 
+create or replace PROCEDURE ACTUALIZAR_PRECIO_PEDIDO (varid NUMBER) IS 
 BEGIN
-  
+
 DECLARE
 cantidadLinea  NUMBER (8,2);
 total NUMBER (10,2);
@@ -11,8 +11,11 @@ CURSOR mycursor IS
 SELECT lp.precio, lp.cantidad, lp.iva 
 FROM PEDIDO_TAB ped, 
 TABLE (SELECT lpedido FROM PEDIDO_TAB WHERE id_pedido = ped.id_pedido)lp
-WHERE ped.id_pedido = ID_PED;
+WHERE ped.id_pedido = varid;
 BEGIN
+    IF (varid is null) or (varid < 1) then
+        RAISE_APPLICATION_ERROR (-20001, 'Error: ID del pedido es nulo o menor que 1');
+    END IF;
     total:= 0;
     for i in mycursor loop
         cantidadLinea := i.precio * i.cantidad;
@@ -20,12 +23,10 @@ BEGIN
         total := total + cantidadLinea;
         cantidadLinea := 0;
     end loop;
-    UPDATE PEDIDO_TAB SET PRECIO = 1 where id_pedido = ID_PED;
-END;
-  
-  
-END ACTUALIZAR_PRECIO_PEDIDO;
+    DBMS_OUTPUT.PUT_LINE('UPDATING precio = '||total||' en PEDIDO_TAB con id = '||varid);
+    UPDATE PEDIDO_TAB SET PRECIO = total where id_pedido = varid;
 
---next
-   --DADO EL NOMBRE DE UNA CIUDAD, MOSTRAR LA INFORMACION. Esta informacion mostrara el nombre de los clientes, asi como el ultimo metodo de pago que tiene cada cliente registrado,
-   --un porcentage de tarjeta/efectuvo y el total recaudad (suma del importe de todos los pedidos)
+END;
+
+
+END;
