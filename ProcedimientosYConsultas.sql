5e62823809dd81721d5a5a7b3ef595884a3a7adb
albertonov <72156084+albertonov@users.noreply.github.com>
2021-04-24 18:24:32 +0200

    Create ProcedimientosYConsultas.sql

---
+++ ProcedimientosYConsultas.sql
@@ -0,0 +1,105 @@
+--Alberto
+
+--Funcion que, dado un numero de pedido, calcula el precio total del pedido, sumando el precio multiplicado por la cantidad y aplicando el porcentaje IVA.
+--Recibe un numero de pedido y devuelve el total.
+
+create or replace FUNCTION CALCULAR_PRECIO_PEDIDO (varid NUMBER) RETURN NUMBER IS 
+BEGIN
+
+DECLARE
+cantidadLinea  NUMBER (8,2);
+total NUMBER (10,2);
+CURSOR mycursor IS
+    SELECT lp.precio, lp.cantidad, lp.iva 
+    FROM PEDIDO_TAB ped, 
+    TABLE (SELECT lpedido FROM PEDIDO_TAB WHERE id_pedido = ped.id_pedido)lp
+    WHERE ped.id_pedido = varid;
+BEGIN
+    IF (varid is null) or (varid < 1) then
+        RAISE_APPLICATION_ERROR (-20001, 'Error: ID del pedido es nulo o menor que 1');
+    END IF;
+    total:= 0;
+    for i in mycursor loop
+        cantidadLinea := i.precio * i.cantidad;
+        cantidadLinea := cantidadLinea + (cantidadLinea*(i.iva/100));
+        total := total + cantidadLinea;
+        cantidadLinea := 0;
+    end loop;
+    RETURN total;
+    
+END;
+
+
+END CALCULAR_PRECIO_PEDIDO;
+
+
+
+create or replace PROCEDURE MOSTRAR_HISTORIAL (correo VARCHAR2) IS 
+    --muestra el historial de pedidos de un cliente
+    TYPE PED_TAB IS TABLE OF REF PEDIDO_OBJ;
+    TPED PED_TAB;
+    
+    cnombre CLIENTE_tab.nombre%TYPE;
+    capellidos CLIENTE_tab.apellidos%TYPE;
+    ctelefono  CLIENTE_tab.telefono%TYPE; 
+    cdireccion CLIENTE_tab.direccion%TYPE;
+
+    pid PEDIDO_TAB.id_pedido%TYPE;
+    pprecio PEDIDO_TAB.precio%TYPE;
+    pfecha PEDIDO_TAB.fecha%TYPE;
+    
+    productoNombre PRODUCTO_TAB.nombre%TYPE;
+    precioCalculado NUMBER(8,2);
+    
+    indiceLinea number(3);
+    BEGIN
+
+    SELECT c.nombre, c.apellidos, c.telefono, c.direccion into  cnombre, capellidos, ctelefono, cdireccion
+    FROM CLIENTE_TAB c WHERE c.correoe = correo;
+        
+    DBMS_OUTPUT.PUT_LINE(cnombre || ' ' || capellidos);
+    DBMS_OUTPUT.PUT_LINE(ctelefono);
+    DBMS_OUTPUT.PUT_LINE(cdireccion);
+
+
+    SELECT REF(p) BULK COLLECT INTO TPED
+    FROM PEDIDO_TAB p
+    WHERE PEDIDO = /*deberia de ser cliente, pero se llama pedido*/ (SELECT REF(c) FROM CLIENTE_TAB c
+                          WHERE correoe = correo);
+
+    FOR I IN 1..TPED.COUNT LOOP
+        indicelinea:=0;
+        SELECT DEREF(TPED(I)).id_pedido, DEREF(TPED(I)).precio, DEREF(TPED(I)).fecha  INTO pid,pprecio, pfecha FROM DUAL;
+        DBMS_OUTPUT.PUT_LINE('--------------------------------------------------');        
+        DBMS_OUTPUT.PUT_LINE('nPedido: ' || pid );
+        DBMS_OUTPUT.PUT_LINE('--------------------------------------------------');
+        DBMS_OUTPUT.PUT_LINE('   	PRODUCTO        CTD     PRECIO      IVA');
+
+        FOR linea in (
+            SELECT lp.*
+            FROM PEDIDO_TAB ped, TABLE (SELECT lpedido FROM PEDIDO_TAB WHERE id_pedido = ped.id_pedido)lp
+            WHERE ped.id_pedido = DEREF(TPED(I)).id_pedido)
+        LOOP
+
+            --Buscamos el nombre del producto
+            SELECT pr.nombre into productoNombre
+            FROM PRODUCTO_TAB pr
+            WHERE id_producto =  DEREF(linea.producto).id_producto;
+
+            indicelinea := indicelinea + 1;
+            DBMS_OUTPUT.PUT_LINE('  ' || indicelinea || chr(9)||productoNombre || CHR(9)||'x'|| linea.cantidad || chr(9)|| linea.precio ||CHR(9)||linea.iva);
+        
+        END LOOP;
+        precioCalculado := CALCULAR_PRECIO_PEDIDO(pid);
+        
+        DBMS_OUTPUT.PUT_LINE(CHR(9)|| 'Fecha '|| pfecha ||CHR(9)||'Precio Total: ' || precioCalculado ||'€');
+        IF ( pprecio != precioCalculado) then
+            DBMS_OUTPUT.PUT_LINE('Precio no coincide, actualizando base de datos');
+            UPDATE PEDIDO_TAB x SET precio = precioCalculado WHERE x.id_pedido = pid;
+        END IF;
+        
+        
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('-');
+    
+END;
