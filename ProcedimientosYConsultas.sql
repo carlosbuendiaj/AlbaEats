bac4b91e49e68cc138e459cec01d31f54e29529d
alfonm8 <61537992+alfonm8@users.noreply.github.com>
2021-04-25 17:00:36 +0200

    Añadido procedimiento CREAR_FACTURA
    Este procedimiento está vinculado el trigger reparacion.

--- ProcedimientosYConsultas.sql
+++ ProcedimientosYConsultas.sql
@@ -139,3 +139,30 @@ END;
 
 
 END DESPEDIR_REPARTIDOR;
+                                        
+                                        
+--Alfonso
+-- Función que creará una nueva factura con los parámetros y relaciones que se le pasen
+CREATE OR REPLACE PROCEDURE CREAR_FACTURA(nid_factura number, ndescripcion varchar2, nimporte number,
+m_dni varchar2, v_matricula varchar2) IS
+FACTURA_N REF FACTURA_OBJ;
+BEGIN
+
+    SELECT REF(M) INTO MECANICO FROM MECANICO_TAB M
+    WHERE DNI = m_dni;
+
+    INSERT INTO FACTURA_OBJ VALUES (nid_factura, ndescripcion, nimporte,
+    (SELECT ref(m) FROM MECANICO_TAB m WHERE dni = m_dni ),
+    (SELECT ref(v) FROM VEHICULO_TAB v WHERE matricula = v_matricula));
+
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('ERROR,EL MECANICO O EL VEHICULO NO EXISTEN');
+
+END CREAR_FACTURA;
+     
+     
+     
+     
+     
+     
